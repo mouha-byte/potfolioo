4c269c85993489049d2e2d93481613ce1f92f394
mouha-byte <mouhanedmliki6@outlook.com>
2025-11-05 15:19:13 +0100

    h

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("/api/send-email", {
+      const response = await fetch("http://localhost:3001/api/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
